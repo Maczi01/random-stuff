1071538dd4eec8f1149c46798f15502cd5116388
Maczi01 <mateusz.w.twardy@gmail.com>
2020-03-28 16:35:38 +0100

    Add more data: first name, email and pets

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const {ApolloServer, gql} = require("apollo-server");
 const typeDefs = gql`
     type Query {
         greeting: String,
-        interestingURLs: [String]
+        interestingURLs: [String],
+        firstName: String,
+        email: String,
+        pets: [String]
     }
 `;
 
 const data = {
     greeting: "Hello World!",
-    interestingURLs: ["https://kursreacta.pl", "www.medium.com"]
+    interestingURLs: ["https://kursreacta.pl", "www.medium.com"],
+    firstName: "John",
+    email: "john@example.com",
+    pets: ["Mittens", "Doggo", "Birb"]
 }
 
-const server = new ApolloServer({typeDefs, rootValue:data});
+const server = new ApolloServer({typeDefs, rootValue: data});
 
 server.listen({port: 4000}).then((result) => console.log(result.url));
